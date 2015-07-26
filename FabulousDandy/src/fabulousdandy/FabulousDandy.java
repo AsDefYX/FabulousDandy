b187b65997431dc9e81e5f1c34c2109a8474edc5
smblr <blood_metal@gmx.net>
2015-07-26 22:12:56 +0200

    3. test

--- FabulousDandy/src/fabulousdandy/FabulousDandy.java
+++ FabulousDandy/src/fabulousdandy/FabulousDandy.java
@@ -16,6 +16,7 @@ public class FabulousDandy {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("greger");
     }
     
 }
