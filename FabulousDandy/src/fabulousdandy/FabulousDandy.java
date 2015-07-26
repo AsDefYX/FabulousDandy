e942b7dee5e9353bd246dac0a58af3fa58164f0c
asdefyx <fabi_live@hotmail.de>
2015-07-26 22:25:30 +0200

    Revert "Sure"
    This reverts commit dccc4f3728e2091aa3352d395edc3aedffbe8967.

--- FabulousDandy/src/fabulousdandy/FabulousDandy.java
+++ FabulousDandy/src/fabulousdandy/FabulousDandy.java
@@ -17,7 +17,6 @@ public class FabulousDandy {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("greger");
-        System.out.println("sureMax");
     }
     
 }
