dccc4f3728e2091aa3352d395edc3aedffbe8967
asdefyx <fabi_live@hotmail.de>
2015-07-26 22:13:55 +0200

    Sure

--- FabulousDandy/src/fabulousdandy/FabulousDandy.java
+++ FabulousDandy/src/fabulousdandy/FabulousDandy.java
@@ -17,6 +17,7 @@ public class FabulousDandy {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("greger");
+        System.out.println("sureMax");
     }
     
 }
